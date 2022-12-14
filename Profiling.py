16e8e82bbb1031b7b11a0e319c702638e1380e11
Carlo <carlo.nicolo@hotmail.com>
2022-12-14 19:51:56 +0100

    Profiling Queue with LinkedList and Queue using standard library python list

---
+++ Profiling.py
@@ -0,0 +1,26 @@
+import cProfile
+from Queue import Queue
+
+
+def profile_queue(n):
+    q = Queue()
+    for i in range(n):
+        q.enqueue(i)
+    while not q.is_empty():
+        q.dequeue()
+
+
+def profile_list_as_queue(n):
+    q = []
+    for i in range(n):
+        q.insert(0, 1)
+    while len(q) > 0:
+        q.pop()
+
+
+def profile(n):
+    profile_queue(n)
+    profile_list_as_queue(n)
+
+
+cProfile.run("profile(100000)")
